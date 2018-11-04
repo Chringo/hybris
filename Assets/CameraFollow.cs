1f81f30485b3f69fe1b59563237084adc35b38d7
Chringo <christofferb93@hotmail.com>
2018-11-04 01:33:24 +0100

    UPDATE Camera to follow target with SmoothDamp

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -1,14 +1,18 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
 
     public Transform target;
-    public float smoothSpeed = 0.125f;
+    public float cameraResponse = 0.1f;
+    public Vector3 offset = new Vector3(0, 0, -10);
+    private Vector3 velocity = Vector3.zero;
 
-    private void LateUpdate()
+    private void Update()
     {
-        transform.position = new Vector3(target.position.x, target.position.y, target.position.z - 10);
+        //Vector3 desiredPosition = target.position + offset;
+        //transform.position = desiredPosition;
+
+        Vector3 targetPosition = target.TransformPoint(offset);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, cameraResponse);
     }
 }
