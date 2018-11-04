ff62e87adb348762316009961c65bb32b6322150
Chringo <christofferb93@hotmail.com>
2018-11-04 23:21:31 +0100

    FIX camera jitter using incorrect Update

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -5,9 +5,10 @@ public class CameraFollow : MonoBehaviour
     public static Transform target;
     public float cameraResponse = 0.1f;
     public Vector3 offset = new Vector3(0, 0, -10);
+    public float cameraSpeed = 10.0f;
     private Vector3 velocity = Vector3.zero;
 
-    private void Update()
+    private void FixedUpdate()
     {
         if (target == null)
         {
@@ -18,7 +19,7 @@ public class CameraFollow : MonoBehaviour
         //transform.position = desiredPosition;
 
         Vector3 targetPosition = target.TransformPoint(offset);
-        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, cameraResponse);
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, cameraResponse, cameraSpeed, Time.fixedDeltaTime);
     }
 
     public static void FindPlayer()
