775454c2512a1c0878dfed81d55bb82168b1fde8
Marc Andersson <marc.r.n.andersson@gmail.com>
2018-11-04 23:50:17 +0100

    ADD Goal Point

---
+++ Assets/EndTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public GameMaster gm;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        gm.LevelCompleted();
+    }
+}
--- Assets/GameMaster.cs
+++ Assets/GameMaster.cs
@@ -6,15 +6,19 @@ public class GameMaster : MonoBehaviour
 {
     public static GameMaster gm;
 
+    public bool gameHasEnded = false;
     public Transform playerPrefab;
     public Transform spawnPoint;
     public int spawnDelay = 2;
+    public int restartDelay = 1;
 
     private void Start()
     {
+        gameHasEnded = false;
+
         if (gm == null)
         {
-            gm = GameObject.FindGameObjectWithTag("Game Master").GetComponent<GameMaster>();
+            gm = GameObject.FindGameObjectWithTag("GameMaster").GetComponent<GameMaster>();
         }
     }
 
@@ -28,5 +32,29 @@ public class GameMaster : MonoBehaviour
     {
         Destroy(player.gameObject);
         gm.StartCoroutine(gm.RespawnPlayer());
+    }
+
+    public void LevelCompleted()
+    {
+        Debug.Log("LEVEL COMPLETED");
+        // EndGame();
+    }
+
+    public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            Restart();
+        }
+    }
+
+    private void Restart()
+    {
+        Debug.Log("RESTART CURRENT SCENE");
+
+        // TODO: Restart the current scene
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().name)
     }
 }
